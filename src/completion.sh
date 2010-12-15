1ad32b36814e5ab5bdf3b92fc49992d7bc3a4ff6
Anders Olsson <anders@nodeone.se>
2010-12-15 08:50:25 +0100

    more plugins are now using api_version 1

--- src/completion.sh
+++ src/completion.sh
@@ -1,5 +1,24 @@
 _vnodectrl() {
-    COMPREPLY=( $(compgen -W "$(${COMP_WORDS[0]} --completion=${COMP_WORDS[1]:-'core'} ${COMP_WORDS[@]:2})" -- ${COMP_WORDS[COMP_CWORD]}) )
+    local c plugin='core' args=''
+
+    if [ $COMP_CWORD -gt 1 ]; then    
+    	c="${COMP_WORDS[1]}"
+
+	    if [[ $c == -i ]]; then
+    	    if [ $COMP_CWORD -gt 2 ]; then
+	            plugin="${COMP_WORDS[3]:-core}"
+	            args=${COMP_WORDS[@]:4}
+    	    else
+	            plugin=''
+	        fi
+	    elif [[ $c == -h|help ]]; then
+	        plugin=''
+    	fi
+	fi
+
+    if [ -n "$plugin" ]; then
+        COMPREPLY=( $(compgen -W "$(${COMP_WORDS[0]} --completion=$plugin $args)" -- ${COMP_WORDS[COMP_CWORD]}) )
+    fi
 }
 
 complete -F _vnodectrl vnodectrl
