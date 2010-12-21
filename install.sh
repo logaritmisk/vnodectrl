449b98cd7a0bcd6c4c8392c6ebccd80d3ed9f213
Anders Olsson <anders@nodeone.se>
2010-12-21 20:57:14 +0100

    removed old code from install and added update function to vnodectrl

--- install.sh
+++ install.sh
@@ -9,39 +9,4 @@ readonly APP_FOLDER=/usr/vnodectrl
 
 /bin/bash -o pipefail -c "/usr/bin/curl -sSfL https://github.com/Logaritmisk/vnodectrl/tarball/2.x-dev | sudo /usr/bin/tar xvz -C$APP_FOLDER --strip 1"
 
-/usr/bin/sudo /bin/ln -sf $APP_FOLDER/bin /usr/bin/vnodectrl
-
-
-
-
-
-
-
-
-
-
-
-
-
-#SRC_FOLDER=$(dirname $0)
-#APP_FOLDER=/opt/vnodectrl.d
-
-
-#if [ ! -d $APP_FOLDER ]; then
-#	sudo mkdir -p $APP_FOLDER
-#fi
-
-#sudo cp -R $SRC_FOLDER/src/* $APP_FOLDER
-
-#sudo chmod +x $APP_FOLDER/vnodectrl
-
-#sudo ln -sf $APP_FOLDER/vnodectrl /usr/bin/
-
-#if [ ! -d $HOME/.vnodectrl.d ]; then
-#	mkdir -p $HOME/.vnodectrl.d/conf.d
-#fi
-
-#if ! grep "source $APP_FOLDER/completion.sh" $HOME/.bashrc > /dev/null; then
-#	echo -e "\nsource $APP_FOLDER/completion.sh" >> $HOME/.bashrc
-#	. $HOME/.bashrc
-#fi
+/usr/bin/sudo /bin/ln -sfn $APP_FOLDER/bin /usr/bin/vnodectrl
