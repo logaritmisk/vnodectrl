c990aad52317d1a9ed0174387785fca6874609ef
Anders Olsson <anders@nodeone.se>
2010-12-21 19:55:52 +0100

    install script is working, i think

--- install.sh
+++ install.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
 
-SRC_FOLDER=$(dirname $0)
-APP_FOLDER=/opt/vnodectrl.d
+readonly APP_FOLDER=/usr/vnodectrl
+
+
+/usr/bin/sudo /bin/mkdir -p $APP_FOLDER
+/usr/bin/sudo /bin/chmod g+w $APP_FOLDER
+
+/bin/bash -o pipefail -c "/usr/bin/curl -sSfL https://github.com/Logaritmisk/vnodectrl/tarball/2.x-dev | sudo /usr/bin/tar xvz -C$APP_FOLDER --strip 1"
+
+/usr/bin/sudo /bin/ln -sf $APP_FOLDER/bin /usr/bin/vnodectrl
+
+
+
+
+
+
+
+
+
+
+
+
+
+#SRC_FOLDER=$(dirname $0)
+#APP_FOLDER=/opt/vnodectrl.d
 
 
 #if [ ! -d $APP_FOLDER ]; then
