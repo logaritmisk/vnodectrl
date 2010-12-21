ca2ea8e01229271b472d770ee339582667e6896a
Anders Olsson <anders@nodeone.se>
2010-12-21 19:22:01 +0100

    new branch, new code

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@ SRC_FOLDER=$(dirname $0)
 APP_FOLDER=/opt/vnodectrl.d
 
 
-if [ ! -d $APP_FOLDER ]; then
-	sudo mkdir -p $APP_FOLDER
-fi
+#if [ ! -d $APP_FOLDER ]; then
+#	sudo mkdir -p $APP_FOLDER
+#fi
 
-sudo cp -R $SRC_FOLDER/src/* $APP_FOLDER
+#sudo cp -R $SRC_FOLDER/src/* $APP_FOLDER
 
-sudo chmod +x $APP_FOLDER/vnodectrl
+#sudo chmod +x $APP_FOLDER/vnodectrl
 
-sudo ln -sf $APP_FOLDER/vnodectrl /usr/bin/
+#sudo ln -sf $APP_FOLDER/vnodectrl /usr/bin/
 
-if [ ! -d $HOME/.vnodectrl.d ]; then
-	mkdir -p $HOME/.vnodectrl.d/conf.d
-fi
+#if [ ! -d $HOME/.vnodectrl.d ]; then
+#	mkdir -p $HOME/.vnodectrl.d/conf.d
+#fi
 
-if ! grep "source $APP_FOLDER/completion.sh" $HOME/.bashrc > /dev/null; then
-	echo -e "\nsource $APP_FOLDER/completion.sh" >> $HOME/.bashrc
-	. $HOME/.bashrc
-fi
+#if ! grep "source $APP_FOLDER/completion.sh" $HOME/.bashrc > /dev/null; then
+#	echo -e "\nsource $APP_FOLDER/completion.sh" >> $HOME/.bashrc
+#	. $HOME/.bashrc
+#fi
--- src/res/guest/sql-restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script to restore all databases one by one from /srv/mysql
-# @author Anders Olsson (logaritmisk)
-
-for DATABASE in $(ls /srv/mysql/*.last.sql.gz); do
-	gunzip -c $DATABASE | mysql -u root
-done
