74ce5956f54009e2d64e9a8f923b5ab6c8a3e97e
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2011-05-18 12:41:25 +0200

    sane placement of vnodectrl conf file

--- src/vnodectrl.py
+++ src/vnodectrl.py
@@ -1,41 +1,48 @@
 import os
+import sys
 import os.path
 from optparse import OptionParser
 import plugins
 import utils
 from plugins import *
 
-modules = []
-commands = {}
-# Locate the configuration.
-current_dir = os.getcwd()
-path = "{0}/vnodectrl.conf".format(current_dir)
-configuration = utils.getConfig(path)
+def main(args):
+	modules = []
+	commands = {}
+	# Locate the configuration.
+	config_file = "{0}/.vnodectrl.d/3.x/vnodectrl.conf".format(os.getenv("HOME"));
+	configuration = utils.getConfig(config_file)
 
-# Locate deployment instructions.
-configuration["deployment"] = utils.getDeploymentConfig()
+	if configuration == False:
+		print "No configuration available. Make sure everything is installed properly"
+		return 1
 
-for plugin in plugins.__all__:
-	module = getattr(plugins, plugin)
-	modules.append(module)
-	for command, options in module.COMMANDS.iteritems():
-		options['module'] = module
-		commands[command] = options
+	# Locate deployment instructions.
+	configuration["deployment"] = utils.getDeploymentConfig()
 
-parser = OptionParser()
-(options, args) = parser.parse_args()
+	for plugin in plugins.__all__:
+		module = getattr(plugins, plugin)
+		modules.append(module)
+		for command, options in module.COMMANDS.iteritems():
+			options['module'] = module
+			commands[command] = options
 
-if len(args) > 0:
-	primary_command = args[0];
-	command_info = commands.get(primary_command, None);
-	if command_info != None:
-		plugin_class = getattr(command_info['module'], command_info['plugin'])
-		plugin = plugin_class(configuration);
-		plugin.execute(primary_command, args, options)
+	parser = OptionParser()
+	(options, args) = parser.parse_args()
+
+	if len(args) > 0:
+		primary_command = args[0];
+		command_info = commands.get(primary_command, None);
+		if command_info != None:
+			plugin_class = getattr(command_info['module'], command_info['plugin'])
+			plugin = plugin_class(configuration);
+			plugin.execute(primary_command, args, options)
+		else:
+			print "Unrecognized command"
+
 	else:
-		print "Unrecognized command"
+		print "Available Commands:\n"
+		for command, options in commands.iteritems():
+			print "{0}\t{1}\n".format(command, options['description']);
 
-else:
-	print "Available Commands:\n"
-	for command, options in commands.iteritems():
-		print "{0}\t{1}\n".format(command, options['description']);
+sys.exit(main(sys.argv))
