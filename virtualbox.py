b511eef672442bc5201a55052c5ee318aa1dbd47
Anders Olsson <anders.e.olsson@gmail.com>
2011-05-18 14:20:43 +0200

    update virtualbox driver and demo

--- virtualbox.py
+++ virtualbox.py
@@ -2,10 +2,39 @@ from time import sleep
 
 import vboxapi
 
-# from libcloud.compute.providers import Provider
 from libcloud.compute.types import NodeState
 from libcloud.compute.base import Node, NodeDriver
 from libcloud.compute.base import NodeSize, NodeImage
+
+
+def machine_loader(fn):
+	def decorate(*largs):
+		return lambda *args: fn(*(largs + args))
+	
+	return decorate
+
+
+@machine_loader
+def machine_from_imachine(imachine, node=None, *args, **kwargs):
+    node._machine = imachine
+    
+    node.name = node._machine.name
+    node.state = NodeState.RUNNING if node._machine.state is 5 else NodeState.PENDING
+    node.public_ip = node._machine.enumerateGuestProperties('*/IP')[1]
+    
+    return True
+
+@machine_loader
+def machine_from_iappliance(iappliance, iprogress, node=None, *args, **kwargs):
+    if not iprogress.completed:
+        return False
+    
+    machines = node.driver._manager.getArray(iappliance, 'machines')
+    machine = machines[0]
+    
+    machine = node.driver._manager.vbox.findMachine(machine)
+    
+    return machine_from_imachine(machine)(node)
 
 
 class VirtualBoxNodeImage(NodeImage):
@@ -20,40 +49,51 @@ class VirtualBoxNodeImage(NodeImage):
         }
         settings.update(kwargs)
         
-        super(VirtualBoxNodeImage, self).__init__(*args, **settings)
+        NodeImage.__init__(self, *args, **settings)
     
 
 class VirtualBoxNode(Node):
-    def __init__(self, machine, *args, **kwargs):
-        self._machine = machine
-        
-        state = NodeState.RUNNING if self._machine.state is 5 else NodeState.PENDING
-        ip = self._machine.enumerateGuestProperties('*/IP')
+    def __init__(self, loader, *args, **kwargs):    
+        self._loader = loader
         
         settings = {
-            'id': self._machine.id,
-            'name':self._machine.name,
-            'state': state,
-            'public_ip': ip[1],
+            'id': '',
+            'name': '',
+            'state': NodeState.UNKNOWN,
+            'public_ip': [],
             'private_ip': [],
         }
         settings.update(kwargs)
         
-        super(VirtualBoxNode, self).__init__(*args, **settings)
+        Node.__init__(self, *args, **settings)
+    
+    def __getattribute__(self, name):
+        value = object.__getattribute__(self, name)
+        
+        if name is '_loader':
+            return value
+        
+        if self._loader:
+            loader, self._loader = self._loader, None
+            
+            if not loader(self):
+                self._loader = loader
+        
+        return value
     
 
 class VirtualBoxNodeDriver(NodeDriver):
     type = 32
     name = "VirtualBox"
     
-    def __init__(self, key='', *args, **kwargs):
-        super(VirtualBoxNodeDriver, self).__init__(key, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        NodeDriver.__init__(self, *args, **kwargs)
         
         self._manager = vboxapi.VirtualBoxManager(None, None)
         self._session = self._manager.mgr.getSessionObject(self._manager.vbox)
     
-    def _to_node(self, machine):
-        return VirtualBoxNode(machine, driver=self.connection.driver)
+    def _to_node(self, loader):
+        return VirtualBoxNode(loader, driver=self.connection.driver)
     
     def _to_image(self, path):
         appliance = self._manager.vbox.createAppliance()
@@ -69,33 +109,21 @@ class VirtualBoxNodeDriver(NodeDriver):
         appliance.read(image._appliance.path)
         appliance.interpret()
         
-        self._manager.getArray(appliance, 'virtualSystemDescriptions')
+        constants = self._manager.constants.all_values('VirtualSystemDescriptionType')
         
         for desc in self._manager.getArray(appliance, 'virtualSystemDescriptions'):
             values = desc.getDescription()
             
-            values[3][1] = name
+            values[constants['Name']][1] = name
             
             desc.setFinalValues([True] * len(values[3]), values[3], values[4])
         
         progress = appliance.importMachines()
         
-        p = -1
-        while not progress.completed:
-            if progress.percent > p:
-                p = progress.percent
-                print '%s%%%s' % (p,  ((' (ETA %s seconds)' % progress.timeRemaining) if p > 20 else ''))
-            
-            sleep(1)
+        if kwargs.get('wait', False):
+            progress.waitForCompletion(kwargs.get('timeout', -1))
         
-        machine = self._manager.getArray(appliance, 'machines')[0]        
-        machine = self._manager.vbox.findMachine(machine)
-        
-        node = self._to_node(machine)
-        
-        print self.startup_node(node)
-        
-        return node
+        return self._to_node(machine_from_iappliance(appliance, progress))
     
     def destroy_node(self, node):
         constants = self._manager.constants.all_values('SessionState')
@@ -113,11 +141,20 @@ class VirtualBoxNodeDriver(NodeDriver):
         return True if progress.resultCode is 0 else False
     
     def reboot_node(self, node):
-        self.shutdown_node(node)
+        self.ex_shutdown_node(node)
         
-        return self.startup_node(node)
+        return self.ex_startup_node(node)
     
-    def startup_node(self, node):
+    def list_nodes(self):
+        return [self._to_node(machine_from_imachine(machine)) for machine in self._manager.getArray(self._manager.vbox, 'machines')]
+    
+    def list_images(self, locations=[]):
+        return [self._to_image(path) for path in locations]
+    
+    def list_sizes(self):
+        return [NodeSize(id='', name='', ram='', disk='', bandwidth=0, price=0, driver=self.connection.driver)]
+    
+    def ex_startup_node(self, node):
         if node._machine.state is 5:
             return node._machine.state
         
@@ -130,7 +167,7 @@ class VirtualBoxNodeDriver(NodeDriver):
         
         return True if progress.resultCode is 0 else False
     
-    def shutdown_node(self, node):
+    def ex_shutdown_node(self, node):
         if node._machine.state is not 5:
             return False
         
@@ -143,13 +180,4 @@ class VirtualBoxNodeDriver(NodeDriver):
         session.unlockMachine()
         
         return True if progress.resultCode is 0 else False
-    
-    def list_nodes(self):
-        return [self._to_node(vm) for vm in self._manager.getArray(self._manager.vbox, 'machines')]
-    
-    def list_images(self, locations=[]):
-        return [self._to_image(path) for path in locations]
-    
-    def list_sizes(self):
-        return [NodeSize(id='', name='', ram='', disk='', bandwidth=0, price=0, driver=self.connection.driver)]
     
