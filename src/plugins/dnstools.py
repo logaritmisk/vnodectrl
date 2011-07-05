ceada32a0e4262fbe0390c98a746ad2e9b170634
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2011-07-05 13:58:46 +0200

    DNS Removal function

--- src/plugins/dnstools.py
+++ src/plugins/dnstools.py
@@ -3,6 +3,7 @@ import dns.zone
 from dns.exception import DNSException
 from dns.rdataclass import *
 from dns.rdatatype import *
+
 COMMANDS = {
 	"dns-record-add" : {
 		"description": "Add DNS record",
@@ -29,11 +30,13 @@ class DNSPlugin(VnodectrlPlugin):
 		"""
 		Main dispatcher for all commands.
 		"""
-		self.zone = dns.zone.from_file(str(self.config['dns']['zone_file']), str(self.config['dns']['domain']));
+		self.zone = dns.zone.from_file(str(self.config['dns']['zone_file']), str(self.config['dns']['domain']))
 		if cmd == "dns-record-list":
 			self.listRecords()
 		elif cmd == "dns-record-add":
 			self.addRecord(args)
+		elif cmd == "dns-record-delete":
+			self.removeRecord(args)
 		
 	def listRecords(self):
 		"""
@@ -44,6 +47,9 @@ class DNSPlugin(VnodectrlPlugin):
 			print name
 	
 	def addRecord(self, args):
+		"""
+		Removes a record from the DNS zone file.
+		"""
 		# We need a few arguments for this to work. Let's make
 		# sure they are all there.
 		if len(args) < 4:
@@ -56,4 +62,14 @@ class DNSPlugin(VnodectrlPlugin):
 		rdataset = self.zone.find_rdataset(name, rdtype=type, create=True)
 		rdata = dns.rdtypes.IN.A.A(IN, A, address=address)
 		rdataset.add(rdata, ttl=86400)
+		self.zone.to_file(self.config['dns']['zone_file'])
+	
+	def removeRecord(self, args):
+		"""
+		Removes a record from the DNS zone file.
+		"""
+		if len(args) < 2:
+			print "You must specify the record you want to remove."
+		name = args[1];
+		self.zone.delete_node(name)
 		self.zone.to_file(self.config['dns']['zone_file'])
