15c4a10c387d1734ac1ff8ee05cef8015ef86752
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2011-07-05 13:53:56 +0200

    Adding record kind of works

--- src/plugins/dnstools.py
+++ src/plugins/dnstools.py
@@ -1,7 +1,8 @@
-import base
-from base import VnodectrlPlugin	
-import bindutil
+from base import VnodectrlPlugin
 import dns.zone
+from dns.exception import DNSException
+from dns.rdataclass import *
+from dns.rdatatype import *
 COMMANDS = {
 	"dns-record-add" : {
 		"description": "Add DNS record",
@@ -29,7 +30,10 @@ class DNSPlugin(VnodectrlPlugin):
 		Main dispatcher for all commands.
 		"""
 		self.zone = dns.zone.from_file(str(self.config['dns']['zone_file']), str(self.config['dns']['domain']));
-		self.listRecords();
+		if cmd == "dns-record-list":
+			self.listRecords()
+		elif cmd == "dns-record-add":
+			self.addRecord(args)
 		
 	def listRecords(self):
 		"""
@@ -38,3 +42,18 @@ class DNSPlugin(VnodectrlPlugin):
 		print "Records in zone:"
 		for name, node in self.zone.nodes.items():
 			print name
+	
+	def addRecord(self, args):
+		# We need a few arguments for this to work. Let's make
+		# sure they are all there.
+		if len(args) < 4:
+			print "You did not specify all the required arguments, please specify them in the following order:\n\
+			<name> <type> <address>"
+		name = args[1]
+		type = args[2]
+		address = args[3]
+		
+		rdataset = self.zone.find_rdataset(name, rdtype=type, create=True)
+		rdata = dns.rdtypes.IN.A.A(IN, A, address=address)
+		rdataset.add(rdata, ttl=86400)
+		self.zone.to_file(self.config['dns']['zone_file'])
